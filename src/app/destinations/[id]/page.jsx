380474951433bd708e16db19b21067b0fa9f23bc
Zabedfolio <zabedfolio@gmail.com>
2026-05-14 14:45:01 +0600

    added

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const DestinationDetailsPage = () => {
+const DestinationDetailsPage = async({params}) => {
+    const {id} = await params;
+    
+    const res = await fetch(`http://localhost:5000/destination/${id}`);
+    const destination = await res.json();
+     console.log(destination)
     return (
         <div>
             Destination details page
